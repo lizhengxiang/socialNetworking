896432459f3585cdc7d0dee76cf1937ccf9e82e2
lizhengxiang <1065482100@qq.com>
2016-10-03 12:07:16 +0800

    个人主页

--- web/personal/js/index.js
+++ web/personal/js/index.js
@@ -39,7 +39,36 @@ function genSearchParams()
     return searchParams;
 }
 
+$('.image-editor').cropit({
+    imageState: {
+        src: 'http://lorempixel.com/500/400/',
+    },
+});
+
+
 $(document).ready(function() {
+
+    $('.image-editor').cropit({
+        imageState: {
+            src: 'http://lorempixel.com/500/400/',
+        },
+    });
+
+    $('.image-editor').cropit();
+
+    $('form').submit(function() {
+        // Move cropped image data to hidden input
+        var imageData = $('.image-editor').cropit('export');
+        $('.hidden-image-data').val(imageData);
+
+        // Print HTTP request params
+        var formValue = $(this).serialize();
+        $('#result-data').text(formValue);
+
+        // Prevent the form from actually submitting
+        return false;
+    });
+
     $("#registered").click(function () {
         var registeredArgs =genSearchParams();
         load('site', 'upload', registeredArgs, function(resultData) {
