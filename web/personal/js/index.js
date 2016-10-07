93b2758ffbf8c6043f9042e007a0c169bfbb6194
lizhengxiang <1065482100@qq.com>
2016-10-07 22:42:15 +0800

    个人主页修改

--- web/personal/js/index.js
+++ web/personal/js/index.js
@@ -23,7 +23,12 @@ $(document).ready(function(){
                 reportNum:'',
                 reportNumTag:'',
                 praiseTag:'',
-                forwardingNumTag:''
+                forwardingNumTag:'',
+                backgroundImg:'',
+                headImg:'',
+                dynamic:'',
+                areLookingAt:'',
+                followers:'',
             },
         },
         created: function() {
@@ -40,6 +45,7 @@ $(document).ready(function(){
                     if(len >= 1){
                         for(var i=0;i<len;i++){
                             self.items.push({
+                                id:resultData[i].id,
                                 speakImg: resultData[i].pic1,
                                 speakImg1:resultData[i].pic2,
                                 speakImg3: resultData[i].pic4,
@@ -81,20 +87,69 @@ $(document).ready(function(){
                 this.information.speakImg2 = tag.speakImg2;
                 this.information.speakImg3 = tag.speakImg3;
                 this.information.nickname = tag.nickname;
-                this.information.school = tag.script;
                 this.information.titleTime = tag.titleTime;
+                this.information.backgroundImg = tag.backgroundImg;
+                this.information.headImg=tag.headImg;
                 this.information.time = tag.time;
                 this.information.content = tag.content;
                 this.information.likeNum = tag.likeNum;
                 this.information.forwardingNum = tag.forwardingNum;
                 this.information.reportNum = tag.reportNum;
                 this.information.reportNumTag=tag.reportNumTag;
+                this.information.school=tag.school;
                 this.information.praiseTag=tag.praiseTag;
                 this.information.forwardingNumTag=tag.forwardingNumTag;
+                this.information.dynamic=50;
+                this.information.areLookingAt=200;
+                this.information.followers=10;
+                $('*').darkTooltip({
+
+                });
             },
-            say: function (msg) {
-                alert(msg)
+            evaluation:function (id,arg,index) {
+                var self = this;
+                load('dynamic', 'evaluation', {'id':id,'arg':arg}, function(resultData) {
+                    //@todo 不能只更新数组的某个属性，带观察，性能影响如何
+                    load('dynamic', 'getdynamic', {'id':id}, function(resultData) {
+                        var len = resultData['data'].length;
+                        resultData = resultData['data'];
+                        if(len >= 1){
+                            for(var i=0;i<len;i++){
+                                self.items.$set(index,{
+                                    id:resultData[i].id,
+                                    speakImg: resultData[i].pic1,
+                                    speakImg1:resultData[i].pic2,
+                                    speakImg3: resultData[i].pic4,
+                                    headImg:resultData[i].headPortrait,
+                                    nickname:resultData[i].nickname,
+                                    backgroundImg:resultData[i].backgroundImage,
+                                    school:resultData[i].school,
+                                    signature:resultData[i].motto,
+                                    dynamic:50,
+                                    areLookingAt:200,
+                                    followers:10,
+                                    time:resultData[i].time,
+                                    titleTime:resultData[i].createtime,
+                                    content:resultData[i].content,
+                                    likeNum:resultData[i].praise,
+                                    reportNum:resultData[i].reportNum,
+                                    forwardingNum:resultData[i].forwardingNum,
+                                    reportNumTag:resultData[i].reportNumTag,
+                                    praiseTag:resultData[i].praiseTag,
+                                    forwardingNumTag:resultData[i].forwardingNumTag
+                                });
+                            }
+                        }else {
+                            layer.msg('获取数据失败', {
+                                offset: 0,
+                                shift: 12
+                            });
+                        }
+                    });
+
+                });
             }
+
         }
     });
 
