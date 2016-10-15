a8d3e4f26a06821a55ed43c7e69498cc87979cff
lizhengxiang <1065482100@qq.com>
2016-10-16 03:19:49 +0800

    优化ｕｒｌ

--- web/registered/js/index.js
+++ web/registered/js/index.js
@@ -1,68 +1,10 @@
-//写入Cookie
-function setCookie(name, value, seconds) {
-    seconds = seconds || 0;   //seconds有值就直接赋值，没有为0，这个根php不一样。
-    var expires = "";
-    if (seconds != 0 ) {      //设置cookie生存时间
-        var date = new Date();
-        date.setTime(date.getTime()+(seconds*1000));
-        expires = "; expires="+date.toGMTString();
-    }
-    document.cookie = name+"="+escape(value)+expires+"; path=/";   //转码并赋值
-}
-//获取Cookie
-function getCookie(name)
-{
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg))
-        return unescape(arr[2]);
-    else
-        return null;
-}
-
-function home() {
-    var href = '/home/index.html';
-    setCookie('url',href,500000);
-    $('iframe').attr('src', href);
-}
-function personal() {
-    var href = '/personal/index.html';
-    setCookie('url',href,500000);
-    $('iframe').attr('src', href);
-}
-
-function mapchat() {
-    var href = '/mapchat/index.html';
-    setCookie('url',href,500000);
-    $('iframe').attr('src', href);
-}
-
-function createactivities() {
-    var href = '/activities/createactivities.html';
-    setCookie('url',href,500000);
-    $('iframe').attr('src', href);
-}
-function activities() {
-    var href = '/activities/index.html';
-    setCookie('url',href,500000);
-    $('iframe').attr('src', href);
-}
 
 function genSearchParams()
 {
     var searchParams = $("#frmSearch").serializeArray();
     return searchParams;
 }
-
-$(function () {
-    if(getCookie('url') == null){
-        var href = '/home/index.html';
-        $('iframe').attr('src', href);
-    }else {
-        var href = getCookie('url');
-        $('iframe').attr('src', href);
-    }
-
-    //注册
+$(document).ready(function() {
     load('registered', 'provinces', {}, function(resultData) {
         var temp = '';
         for(var i =0; i<resultData.length; i++){
@@ -81,7 +23,6 @@ $(function () {
             $("#school").append(temp);
         });
     });
-
     var year = '',mouth='',day='';
     for(var i = 1980; i< 2016; i++){
         year += '<option value ='+i+'>'+i+'</option>';
@@ -143,6 +84,4 @@ $(function () {
             });
         }
     });
-    
-    
-});
+})
